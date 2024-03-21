256c0394218026b8abda5e38bbc6b00e4ba74ef0
Maruf <marufnegmatov107@gmail.com>
2024-03-21 10:55:55 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+// function get(str,n){                      //task1
+//    let s = str.at(-1).repeat(n-1)
+//    return str+s
+// }
+// console.log(get("Hello",3));
+
+
+
+
+// function get(str){                           //task2
+  
+//     return str.at(0) + str.at(-1)
+// }
+// console.log(get("Maruf"));
+
+
+
+
+
+// function get(str){                             //task3
+   
+//     if(str == typeof 77) return toString
+
+//     else return + str
+// }
+// console.log(get("77"));
+
+
+
+
+// function get(str){                          //task3
+   
+//     if(typeof str == typeof 55 && str % 2 == 0) return str.toString()
+
+//     else return  false
+
+// }
+// console.log(get(8));
+
+
+
+
+
+// function get(str){                       //task4
+
+//     if(str.at(-1)=='s') return true
+//     else return false
+
+// }
+// console.log(get("Changes"));
+
+
+
+
+// function get(a){                      //task5
+   
+//     let n  = ''
+//     for(let i =a.length-1;i>=0;i--){
+        
+//         n+=a[i]
+//     }
+//     return n.toUpperCase()
+
+// }
+// console.log(get("abc"));
+
+
+
+
+
+
+
+
